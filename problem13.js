18f76e5f93f6038c1d869c2c5f9ab2754ca8950d
devasenak166 <121279794+devasenak166@users.noreply.github.com>
2023-03-14 20:53:17 +0530

    Delete problem13.js

--- problem13.js
+++
@@ -1,8 +0,0 @@
-let number = +prompt("enter the number:");
-square_root = number ** 0.5;
-// console.log(square_root);
-if (square_root * square_root == number) {
-  console.log("Perfect square");
-} else {
-  console.log("Not a perfect square");
-}
