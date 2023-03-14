bc362aced7f35d56b13f5d34214962f51c0eb33e
devasenak166 <121279794+devasenak166@users.noreply.github.com>
2023-03-14 20:55:23 +0530

    Delete program23.js

--- program23.js
+++
@@ -1,8 +0,0 @@
-let word = prompt("enter the word");
-let wordReverse = word.split("").reverse().join("");
-// console.log(word.split("").reverse().join(""));
-if (word == wordReverse) {
-  console.log("palindrome");
-} else {
-  console.log("not a palindrome");
-}
